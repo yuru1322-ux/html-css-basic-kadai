c8d22a7cda809366bf70998a71c864a5d6c5e7df
名倉　由華 <yuru1322@gmail.com>
2026-06-29 21:48:31 +0900

    Delete SAMURAI GIM/images/script.js

--- SAMURAI GIM/images/script.js
+++
@@ -1,6 +0,0 @@
-const menuBtn = document.querySelector(".nav-s");
-const spMenu = document.querySelector(".s-menu");
-menuBtn.addEventListener("click", function () {
-  spMenu.classList.toggle("open");
-  menuBtn.classList.toggle("open");
-});
